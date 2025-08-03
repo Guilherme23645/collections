c54d5ac778499571864684b2fdf7c7893a8d06e7
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-03 03:22:43 +0000

    feat: add User class

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private int code;
+    private String name;
+
+    public User(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    } 
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
