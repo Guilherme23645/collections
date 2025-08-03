f9f0e701e4aef9211a97b78de5c79d0129e109a5
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-03 03:03:44 +0000

    feat: ArrayList implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<Integer> test = new ArrayList<>();
+        test.add(4);
+        test.add(7);
+        test.add(8);
+        test.add(5);
+        test.remove(2);
+        System.out.println(test);
+        System.out.printf("Array's length: %s%n", test.size());
+        test.forEach(System.out::println);
     }
 }
