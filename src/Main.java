01a9ae070fe9eadf123a0b00ca580ea80f34cdd9
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-03 03:49:08 +0000

    feat: implement ArrayLIst made only of User objects

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> test = new ArrayList<>();
-        test.add(4);
-        test.add(7);
-        test.add(8);
-        test.add(5);
-        test.remove(2);
-        System.out.println(test);
-        System.out.printf("Array's length: %s%n", test.size());
-        test.forEach(System.out::println);
+        List<User> users = new ArrayList<>();
+        var user = new User(143,"Regina");
+        users.add(user);
+        users.add(new User(123, "Julius"));
+        System.out.println(users.contains(user));
+        System.out.println(users.get(0));
+        
     }
 }
