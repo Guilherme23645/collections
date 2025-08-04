1aad6d430f5258ec7fc80a99342d13c47d4548b4
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-04 22:39:00 +0000

    feat: implement Set interface

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        var user = new User(143,"Regina");
-        users.add(user);
-        users.add(new User(123, "Julius"));
-        System.out.println(users.contains(user));
-        System.out.println(users.get(0));
-        System.out.println(user.equals(new User(143,"Regina")));
+        Set<Integer> nums = new HashSet<>();
+        nums.add(2);
+        nums.add(3);
+        nums.add(3);
+        nums.add(4);
+        System.out.println(nums);
     }
 }
