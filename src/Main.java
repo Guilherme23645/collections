52ca47c4031e04da3b4b5ed39381c0d87d8bffe0
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-04 22:44:06 +0000

    feat: implement Map interface

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> nums = new HashSet<>();
-        nums.add(2);
-        nums.add(3);
-        nums.add(3);
-        nums.add(4);
-        System.out.println(nums);
+        Map<String,Integer> shoplist = new LinkedHashMap<>();
+        shoplist.put("bread",10);
+        shoplist.put("apples",6);
+        shoplist.put("bananas",12);
+        shoplist.put("watermelon",2);
+        shoplist.put("orange",5);
+        shoplist.put("milk",2);
+        shoplist.forEach((item, amount) -> System.out.printf("%s: %s%n",item,amount));
     }
 }
