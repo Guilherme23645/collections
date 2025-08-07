822d9799509b2d280c4649a25f06e38c5f1dd1bc
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-07 23:30:42 +0000

    feat: display BigDecimal objects and their operations

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import java.util.*;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> nums = new HashSet<>();
-        nums.add(2);
-        nums.add(3);
-        nums.add(3);
-        nums.add(4);
-        System.out.println(nums);
+        var value1 = new BigDecimal("0.1");
+        var value2 = new BigDecimal("0.2");
+
+        System.out.println(value1.add(value2));
+
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.1")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.2")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("2")));
     }
 }
