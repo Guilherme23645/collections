1e616618d01ce47c0441673373b8e22b8b9ac3a4
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-07 23:32:35 +0000

    feat: check multiply for both values

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
         var value1 = new BigDecimal("0.1");
         var value2 = new BigDecimal("0.2");
 
-        System.out.println(value1.add(value2));
-
-        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.1")));
-        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.2")));
-        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("2")));
+        System.out.println(value1.multiply(value2));
     }
 }
