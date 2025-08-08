9c009133f38f88e4ea09294d93a748e8b8a50a1e
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-08 01:16:32 +0000

    feat: display User

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import java.math.BigDecimal;
+import java.util.Optional;
+
+import domain.User;
+
+import static domain.SexEnum.MALE;
 
 public class Main {
     public static void main(String[] args) {
-        var value1 = new BigDecimal("0.1");
-        var value2 = new BigDecimal("0.2");
-
-        System.out.println(value1.multiply(value2));
+        /*
+        Optional enables NullPointerException handling by return Optional.empty() instead of null
+        To be used when data retrieval methods return null
+        */ 
+        Optional<User> optional = Optional.of(new User("Lloyd",19,MALE));
+        optional.ifPresent(System.out::println);
     }
 }
